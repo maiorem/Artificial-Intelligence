40ceec671e783ae077ce99718877ef5109aeefa4
Seyoung Hong <maiorem00@gmail.com>
2020-11-11 15:59:22 +0900

    ensemble model

--- Study/keras/keras16_ensemble2.py
+++ Study/keras/keras16_ensemble2.py
@@ -1,24 +1,23 @@
 #1. 데이터
 import numpy as np   
 
-x=np.array([range(1, 101), range(311, 411), range(100)])
-x1=np.transpose(x)
-y=np.array([range(101, 201), range(711, 811), range(100)])
-y1=np.transpose(y)
-
-
+x1=np.array([range(1, 101), range(311, 411), range(100)])
 x2=np.array([range(4, 104), range(761, 861), range(100)])
-x2=np.transpose(x2)
+
+y1=np.array([range(101, 201), range(711, 811), range(100)])
 y2=np.array([range(501, 601), range(431, 531), range(100, 200)])
+y3=np.array([range(501, 601), range(431, 531), range(100, 200)])
+
+x1=np.transpose(x1)
+x2=np.transpose(x2)
+y1=np.transpose(y1)
 y2=np.transpose(y2)
+y3=np.transpose(y3)
 
 from sklearn.model_selection import train_test_split
 
-x1_train, x1_test, y1_train, y1_test=train_test_split(x1, y1, test_size=0.2)
-x1_train, x1_val, y1_train, y1_val=train_test_split(x1_train, y1_train, test_size=0.2)
-
-x2_train, x2_test, y2_train, y2_test=train_test_split(x2, y2, test_size=0.2)
-x2_train, x2_val, y2_train, y2_val=train_test_split(x2_train, y2_train, test_size=0.2)
+x1_train, x1_test, y1_train, y1_test=train_test_split(x1, y1, test_size=0.3)
+x2_train, x2_test, y2_train, y2_test, y3_train, y3_test=train_test_split(x2, y2, y3, test_size=0.3)
 
 
 #2. 모델 구성
@@ -66,20 +65,24 @@ output2_2=Dense(200, name='output2_2')(output2_1)
 output2_3=Dense(15, name='output2_3')(output2_2)
 output2_4=Dense(3, name='output2_4')(output2_3)
 
+output3_1=Dense(100, name='output3_1')(middle1)
+output3_2=Dense(500, name='output3_2')(output3_1)
+output3_3=Dense(20, name='output3_3')(output3_2)
+output3_4=Dense(3, name='output3_4')(output3_3)
+
 ######### 모델 정의
-model=Model(inputs=[input1, input2], outputs=[output1_3, output2_4])
+model=Model(inputs=[input1, input2], outputs=[output1_3, output2_4, output3_4])
 
 model.summary()
 
 
 #3. 컴파일, 훈련
 model.compile(loss="mse", optimizer="adam")
-model.fit([x1_train, x2_train], [y1_train, y2_train], epochs=100, batch_size=8, 
-        validation_data=([x1_val, x2_val], [y1_val, y2_val]), verbose=1)
+model.fit([x1_train, x2_train], [y1_train, y2_train, y3_train], epochs=100, batch_size=8, 
+        validation_split=0.3, verbose=1)
 
 
 
-
-result=model.evaluate([x1_test, x2_test], [y1_test, y2_test], batch_size=8)
+result=model.evaluate([x1_test, x2_test], [y1_test, y2_test, y3_test], batch_size=8)
 
 print("result : ", result)
