f5f1d255a507c76e6f1e81a4515d6a8ade1b118f
Seyoung Hong <maiorem00@gmail.com>
2020-11-12 19:03:46 +0900

    2020.11.12. 수업 주석 : 조기종료 함수

--- Study/keras/keras21_LSTM_earlyStopping.py
+++ Study/keras/keras21_LSTM_earlyStopping.py
@@ -1,3 +1,5 @@
+#Early Stopping : 훈련을 시키는 도중, 가장 낮은 loss 값이 나왔을 때 훈련을 멈추고 결과값을 출력하도록 하는 함수. patience 값을 조절하여 낮은 loss보다 더 낮은 loss를 찾기 위해 현재의 저장값을 넘길 수 있다. 
+
 import numpy as np     
 
 x=np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],
@@ -13,14 +15,6 @@ x_input=x_input.reshape(1,3,1)
 from tensorflow.keras.models import Sequential, Model 
 from tensorflow.keras.layers import Dense, LSTM, Input
 
-# model=Sequential()
-# model.add(LSTM(40, activation='relu', input_length=3, input_dim=1))
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(500, activation='relu'))
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(80, activation='relu'))
-# model.add(Dense(20, activation='relu'))
-# model.add(Dense(1))
 
 input1=Input(shape=(3,1))
 layer1=LSTM(200, activation='relu')(input1)
@@ -37,10 +31,10 @@ model.compile(loss='mse', optimizer='adam')
 
 #조기종료
 from tensorflow.keras.callbacks import EarlyStopping
-#early_stopping=EarlyStopping(monitor='loss', patience=100, mode='min')
+#early_stopping=EarlyStopping(monitor='loss', patience=100, mode='min') #monitor를 loss로 설정했기 때문에 mode는 가장 낮은 값을 찾도록 해 줌. 만약 accuracy라면 max일 것. 헷갈리면 auto로 해도 됨.
 early_stopping=EarlyStopping(monitor='loss', patience=50, mode='auto')
 
-model.fit(x, y, epochs=10000, batch_size=1, callbacks=[early_stopping])
+model.fit(x, y, epochs=10000, batch_size=1, callbacks=[early_stopping]) #여기서 파라미터로 조기종료 설정을 불러온다.
 
 y_predict=model.predict(x_input)
 
