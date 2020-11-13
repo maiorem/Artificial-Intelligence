5df547604170e76fd4b1284c843b776ea744393f
Seyoung Hong <maiorem00@gmail.com>
2020-11-13 17:06:52 +0900

    데이터 전처리

--- Study/keras/keras34_minmax.py
+++ Study/keras/keras34_minmax.py
@@ -36,38 +36,35 @@ print(x_predict)
 print(x_predict2)
 
 
-
 #StandardScaler
 #RobustScaler
 #MaxAbsScaler
 
+x=x.reshape(14,3,1)
+x_predict=x_predict.reshape(1,3,1)
+
+input1=Input(shape=(3,1))
+dense1=LSTM(180, activation='relu')(input1)
+dense2=Dense(300, activation='relu')(dense1)
+dense3=Dense(8000, activation='relu')(dense2)
+dense4=Dense(2000, activation='relu')(dense3)
+dense5=Dense(600, activation='relu')(dense4)
+dense6=Dense(100)(dense5)
+dense7=Dense(60)(dense6)
+output1=Dense(1)(dense7)
+model=Model(inputs=input1, outputs=output1)
+
+model.summary()
+
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
 
-# x=x.reshape(14,3,1)
-# x_predict=x_predict.reshape(1,3,1)
+early_stopping=EarlyStopping(monitor='loss', patience=50, mode='min')
+
+model.fit(x, y, epochs=10000, batch_size=1, verbose=2, callbacks=[early_stopping])
+
+y_predict=model.predict(x_predict)
 
 
-# input1=Input(shape=(3,1))
-# dense1=LSTM(180, activation='relu')(input1)
-# dense2=Dense(120, activation='relu')(dense1)
-# dense3=Dense(90, activation='relu')(dense2)
-# dense4=Dense(60, activation='relu')(dense3)
-# dense5=Dense(10, activation='relu')(dense4)
-# dense6=Dense(5)(dense5)
-# output1=Dense(1)(dense6)
-# model=Model(inputs=input1, outputs=output1)
-
-# model.summary()
-
-# model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-
-
-# early_stopping=EarlyStopping(monitor='loss', patience=40, mode='min')
-
-# model.fit(x, y, epochs=10000, batch_size=1, verbose=2, callbacks=[early_stopping])
-
-# y_predict=model.predict(x_predict)
-
-
-# print("y_predict : ", y_predict)
-# # print("loss : ", loss)
+print("y_predict : ", y_predict)
+# print("loss : ", loss)
