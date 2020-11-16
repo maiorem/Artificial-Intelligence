2002c434be7694de4bb67e0a4c9b0e401cb733e4
Seyoung Hong <maiorem00@gmail.com>
2020-11-16 16:21:57 +0900

    one hot encoding

--- Study/keras/keras36_mnist2_cnn.py
+++ Study/keras/keras36_mnist2_cnn.py
@@ -9,13 +9,20 @@ from tensorflow.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test)=mnist.load_data()
 
-print(x_train.shape, x_test.shape) #(60000,24,28), (10000,28,28)
+print(x_train.shape, x_test.shape) #(60000,28,28), (10000,28,28)
 print(y_train.shape, y_test.shape) #(60000,) (10000,)
+
+x_predict=x_test[:10, :, :]
+print(x_predict.shape)
+print(x_predict)
+
+
+
 # print(x_train[500])
 # print(y_train[500])
 
-# plt.imshow(x_train[500], 'Blues')
-# plt.show()
+plt.imshow(x_train[500], 'Blues')
+plt.show()
 
 #1. 데이터
 #다중 분류 데이터 전처리 (1).OneHotEncoding
@@ -37,13 +44,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 model=Sequential()
-model.add(Conv2D(10, (2,2), padding='same', input_shape=(28,28,1)))
-model.add(Conv2D(20, (2,2), padding='valid'))
-model.add(Conv2D(30, (3,3)))
-model.add(Conv2D(40, (2,2), strides=2))
+model.add(Conv2D(3, (2,2), padding='same', input_shape=(28,28,1)))
+model.add(Conv2D(10, (2,2), padding='valid'))
+model.add(Conv2D(20, (3,3)))
+model.add(Conv2D(30, (2,2), strides=2))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Flatten())
-model.add(Dense(100, activation='relu'))
+model.add(Dense(20, activation='relu'))
 model.add(Dense(10, activation='softmax')) #softmax=분류값
 #(2). 다중 분류의 output layer의 활성화함수는 softmax를 쓴다.
 
@@ -52,10 +59,31 @@ model.summary()
 #3. 컴파일, 훈련
 #(3). 다중분류에선 반드시 loss를 categorical_crossentropy로 쓴다. 이걸로 이제 accuracy를 잡아줄 수 있다.
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=30, batch_size=32, verbose=1, validation_split=0.2)
+
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+es=EarlyStopping(monitor='loss', patience=50, mode='auto')
+to_hist=TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
+
+model.fit(x_train, y_train, epochs=10000, batch_size=32, verbose=1, validation_split=0.2, callbacks=[es, to_hist])
 
 #4. 평가, 예측
 loss, accuracy=model.evaluate(x_test, y_test, batch_size=32)
 
 print('loss : ', loss)
 print('accuracy : ', accuracy)
+
+'''
+실습 1. 테스트 데이터를 10개 가져와서 predict 만들 것 (원핫 인코딩 원복할 것)
+실습 2. 모델에 es, tensorboard 적용. 
+'''
+
+x_predict=x_predict.reshape(10, 28, 28,1).astype('float32')/255.
+print(x_predict)
+
+
+y_predict=model.predict(x_predict)
+y_predict=np.argmax(y_predict, axis=1)
+y_actually=np.argmax(y_test[:10, :], axis=1)
+print('실제값 : ', y_actually)
+print('예측값 : ', y_predict)
+
