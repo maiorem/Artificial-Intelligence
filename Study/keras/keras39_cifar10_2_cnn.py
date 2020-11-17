a833f257efb54709976e548e1034d7de9cd79dd2
Seyoung Hong <maiorem00@gmail.com>
2020-11-17 10:38:35 +0900

    cifar10

---
+++ Study/keras/keras39_cifar10_2_cnn.py
@@ -0,0 +1,51 @@
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Conv2D, LSTM
+from tensorflow.keras.layers import MaxPooling2D, Flatten
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_predict=x_test[:10, :, :, :]
+
+
+y_train=to_categorical(y_train) 
+y_test=to_categorical(y_test)
+
+print(y_train.shape)
+print(y_test.shape)
+
+
+
+model=Sequential()
+model.add(Conv2D(3, (2,2), padding='same', input_shape=(32,32,3)))
+model.add(Conv2D(10, (2,2), padding='valid'))
+model.add(Conv2D(20, (3,3)))
+model.add(Conv2D(30, (2,2), strides=2))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(20, activation='relu'))
+model.add(Dense(10, activation='softmax')) 
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+es=EarlyStopping(monitor='loss', patience=10, mode='auto')
+# to_hist=TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
+
+model.fit(x_train, y_train, epochs=1000, batch_size=32, verbose=1, validation_split=0.2, callbacks=[es])
+
+#4. 평가, 예측
+loss, accuracy=model.evaluate(x_test, y_test, batch_size=32)
+
+print('loss : ', loss)
+print('accuracy : ', accuracy)
+
+y_predict=model.predict(x_predict)
+y_predict=np.argmax(y_predict, axis=1) #One hot encoding의 decoding은 numpy의 argmax를 사용한다.
+y_actually=np.argmax(y_test[:10, :], axis=1)
+print('실제값 : ', y_actually)
+print('예측값 : ', y_predict)
