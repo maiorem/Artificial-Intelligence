2729232aba7fe5b8be56733be6961543336406bc
Seyoung Hong <maiorem00@gmail.com>
2020-11-17 12:13:00 +0900

    cifar lstm

--- Study/keras/keras39_cifar10_4_lstm.py
+++ Study/keras/keras39_cifar10_4_lstm.py
@@ -21,25 +21,22 @@ y_test=to_categorical(y_test)
 
 model=Sequential()
 model.add(LSTM(20, activation='relu', input_shape=(32*32, 3)))
+model.add(Dense(300, activation='relu'))
 model.add(Dense(200, activation='relu'))
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(550, activation='relu'))
-model.add(Dense(190))
-model.add(Dense(30))
-model.add(Dense(10, activation='softmax')) 
-#(2). 다중 분류의 output layer의 활성화함수는 softmax를 쓴다.
+model.add(Dense(150, activation='relu'))
+model.add(Dense(90, activation='relu'))
+model.add(Dense(30, activation='relu'))
+model.add(Dense(10, activation='softmax'))  
 
 model.summary()
 
-#3. 컴파일, 훈련
-#(3). 다중분류에선 반드시 loss를 categorical_crossentropy로 쓴다. 이걸로 이제 accuracy를 잡아줄 수 있다.
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
-es=EarlyStopping(monitor='loss', patience=10, mode='auto')
+es=EarlyStopping(monitor='loss', patience=3, mode='auto')
 # to_hist=TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
 
-model.fit(x_train, y_train, epochs=1000, batch_size=2000, verbose=1, validation_split=0.2, callbacks=[es])
+model.fit(x_train, y_train, epochs=50, batch_size=2000, verbose=1, validation_split=0.2, callbacks=[es])
 
 #4. 평가, 예측
 loss, accuracy=model.evaluate(x_test, y_test, batch_size=2000)
