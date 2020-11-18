0520754aca5f8f31538afce21b8a6af6cd95a021
Seyoung Hong <maiorem00@gmail.com>
2020-11-18 09:49:16 +0900

    load_iris

---
+++ Study/keras/keras45_iris_3_lstm.py
@@ -0,0 +1,73 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Conv2D, Flatten
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+dataset=load_iris()
+x=dataset.data
+y=dataset.target
+# print(x)
+# print(x.shape, y.shape) #(150, 4) (150,)
+
+
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2)
+
+
+scaler=StandardScaler()
+scaler.fit(x_train)
+x_train=scaler.transform(x_train)
+x_test=scaler.transform(x_test)
+
+
+x_train=x_train.reshape(x_train.shape[0], x_train.shape[1],1)
+x_test=x_test.reshape(x_test.shape[0],x_test.shape[1],1)
+
+model=Sequential()
+model.add(LSTM(80, activation='relu', input_shape=(4, 1)))
+model.add(Dense(150, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(350, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(700, activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(480, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(280, activation='relu'))
+model.add(Dense(80))
+model.add(Dense(30))
+model.add(Dense(1))
+
+
+model.summary()
+
+model.compile(loss='mse', optimizer='adam')
+
+
+early_stopping=EarlyStopping(monitor='loss', patience=50, mode='min')
+
+model.fit(x_train, y_train, epochs=10000, batch_size=1, validation_split=0.2 ,callbacks=[early_stopping])
+
+y_predict=model.predict(x_test)
+
+
+from sklearn.metrics import mean_squared_error 
+def RMSE(y_test, y_pred) :
+    return np.sqrt(mean_squared_error(y_test, y_pred))
+
+print("RMSE : ", RMSE(y_test, y_predict))
+
+from sklearn.metrics import r2_score 
+r2=r2_score(y_test, y_predict)
+print("R2 : ", r2)
+
+
+'''
+load_iris lstm
+RMSE :  0.027598345532891148
+R2 :  0.9989101720055047
+'''
