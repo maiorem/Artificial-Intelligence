0946258d5d5eb17fb25c7de2f3bc191b2d787536
Seyoung Hong <maiorem00@gmail.com>
2020-11-23 20:17:25 +0900

    로드 데이터 수정

--- Study/practice1_samsung/keras_load_MondayStock_NoEnsemble.py
+++ Study/practice1_samsung/keras_load_MondayStock_NoEnsemble.py
@@ -3,21 +3,17 @@ from tensorflow.keras.models import load_model
 from sklearn.model_selection import train_test_split
 
 ######### 1. 데이터
-big_x=np.load('./data/monday/big_x.npy')
-big_x_predict=np.load('./data/monday/big_x_predict.npy')
-big_y=np.load('./data/monday/big_y.npy')
+big_x=np.load('./data/monday/lstm_big_x.npy')
+big_x_predict=np.load('./data/monday/lstm_big_x_predict.npy')
+big_y=np.load('./data/monday/lstm_big_y.npy')
 
 
 # train, test 분리
 big_x_train, big_x_test, big_y_train, big_y_test=train_test_split(big_x, big_y, train_size=0.8)
 
 
-big_x_predict=big_x_predict.reshape(1,18)
-
-
-
 ######### 2. DNN 회귀모델
-model = load_model('./model/samsung-noensemble-34-1165859.8750.hdf5')
+model = load_model('./model/samsung-noensemble-lstm-270-1255467.1250.hdf5')
 
 
 #4. 평가, 예측
