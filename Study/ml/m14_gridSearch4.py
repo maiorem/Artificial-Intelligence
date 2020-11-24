d148382e7bb43c4ff418e38830517cd518ac7738
Seyoung Hong <maiorem00@gmail.com>
2020-11-24 12:12:31 +0900

    Grid Search : regressor and classifier

---
+++ Study/ml/m14_gridSearch4.py
@@ -0,0 +1,45 @@
+#보스턴
+import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, r2_score
+# from sklearn.utils.testing import all_estimators
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+import warnings
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score, r2_score
+
+x, y=load_boston(return_X_y=True)
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8)
+
+
+parameters= [
+    {'n_estimators' : [100,200],
+    'max_depth' : [6,8,10,12],
+    'max_features' : [2, 5],
+    'min_samples_leaf' : [3,5,7,10],
+    'min_samples_split' : [2,3,5,10],
+    'n_jobs' : [-1]}
+] #256번
+
+#2. 모델
+kfold=KFold(n_splits=3, shuffle=True) # 5번
+model=GridSearchCV(RandomForestRegressor(), parameters, cv=kfold) # 총 1280번 훈련
+
+
+#3. 훈련
+model.fit(x_train, y_train) 
+
+#4. 평가, 예측
+print("최적의 매개변수 : ", model.best_estimator_)
+y_predict=model.predict(x_test)
+print('최종정답률 : ', r2_score(y_test, y_predict))
+
+'''
+최적의 매개변수 :  RandomForestRegressor(max_depth=10, max_features=5, min_samples_leaf=3,
+                      n_jobs=-1)
+최종정답률 :  0.9358173849025814
+'''
---
+++ Study/ml/m14_gridSearch5.py
@@ -0,0 +1,43 @@
+#wine
+import numpy as np
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, r2_score
+# from sklearn.utils.testing import all_estimators
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+import warnings
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score, r2_score
+
+x, y=load_wine(return_X_y=True)
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8)
+
+
+parameters= [
+    {'n_estimators' : [100,200],
+    'max_depth' : [6,8,10,12],
+    'min_samples_leaf' : [3,5,7,10],
+    'min_samples_split' : [2,3,5,10],
+    'n_jobs' : [-1]}
+] #128번
+
+#2. 모델
+kfold=KFold(n_splits=3, shuffle=True) # 5번
+model=GridSearchCV(RandomForestClassifier(), parameters, cv=kfold) # 총 640번 훈련
+
+
+#3. 훈련
+model.fit(x_train, y_train) 
+
+#4. 평가, 예측
+print("최적의 매개변수 : ", model.best_estimator_)
+y_predict=model.predict(x_test)
+print('최종정답률 : ', accuracy_score(y_test, y_predict))
+
+'''
+최적의 매개변수 :  RandomForestClassifier(max_depth=10, min_samples_leaf=3, n_jobs=-1)
+최종정답률 :  0.9444444444444444
+'''
