be7d7b8ae0cb6533888fadccf3ebfe9431bcba52
Seyoung Hong <maiorem00@gmail.com>
2020-11-24 16:05:32 +0900

    Update m17_pipeline_wine.py

--- Study/ml/m17_pipeline_wine.py
+++ Study/ml/m17_pipeline_wine.py
@@ -16,17 +16,18 @@ x=wine.drop('quality', axis=1)
 x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8)
 
 parameters= [
-    {'jin__n_estimators' : [100,200],
-    'jin__max_depth' : [6,8,10,12],
-    'jin__min_samples_leaf' : [3,5,7,10],
-    'jin__min_samples_split' : [2,3,5,10],
-    'jin__n_jobs' : [-1]}
+    {'jin__n_estimators' : [100,150,200,300],
+    'jin__max_depth' : [6,8,10,12,15],
+    'jin__min_samples_leaf' : [3,5,7,10,15,20],
+    'jin__min_samples_split' : [2,3,5,10,13],
+    'jin__n_jobs' : [-1],
+    'jin__max_features' : ['auto', 'sqrt', 'log2']}
 ] 
 
 #2. 모델
-pipe=Pipeline([("scaler", RobustScaler()), ('jin', RandomForestClassifier())])
+pipe=Pipeline([("scaler", MinMaxScaler()), ('jin', RandomForestClassifier())])
 # pipe=make_pipeline(MinMaxScaler(), SVC()) 
-model=RandomizedSearchCV(pipe, parameters, cv=5)
+model=RandomizedSearchCV(pipe, parameters, cv=10, verbose=2)
 
 #3. 훈련
 model.fit(x_train, y_train)
@@ -36,9 +37,10 @@ print('acc : ', model.score(x_test, y_test))
 print("최적의 매개변수 : ", model.best_estimator_)
 
 '''
-acc :  0.6448979591836734
-최적의 매개변수 :  Pipeline(steps=[('scaler', RobustScaler()),
+acc :  0.6816326530612244
+최적의 매개변수 :  Pipeline(steps=[('scaler', MinMaxScaler()),
                 ('jin',
-                 RandomForestClassifier(max_depth=12, min_samples_leaf=3,
-                                        n_estimators=200, n_jobs=-1))])
+                 RandomForestClassifier(max_depth=15, min_samples_leaf=3,
+                                        min_samples_split=3, n_estimators=200,
+                                        n_jobs=-1))])
 '''
