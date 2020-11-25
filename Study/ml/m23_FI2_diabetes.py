6a0fc000c3cd35768b20dedf058fbffbdbca13cd
Seyoung Hong <maiorem00@gmail.com>
2020-11-25 15:00:47 +0900

    Update m23_FI2_diabetes.py

--- Study/ml/m23_FI2_diabetes.py
+++ Study/ml/m23_FI2_diabetes.py
@@ -13,17 +13,6 @@ dataset=load_diabetes()
 x=dataset.data
 y=dataset.target
 
-
-# #PCA로 컬럼 걸러내기
-# pca=PCA()
-# pca.fit(x)
-# cumsum=np.cumsum(pca.explained_variance_ratio_) #누적된 합 표시
-# # print(cumsum)
-
-# d=np.argmax(cumsum >= 0.8) + 1
-# # print(cumsum>=0.95) 
-# print(d) # 2 4
-
 pca1=PCA(n_components=7)
 x=pca1.fit_transform(x)
 
@@ -34,18 +23,18 @@ model.fit(x_train, y_train)
 
 y_predict=model.predict(x_test)
 
-print("score :", r2_score(y_test, y_predict))
+print("score :", model.score(x_test, y_test))
 print(model.feature_importances_)
 
 '''
 Default
-score : 0.02247191011235955
-[0.11618966 0.10648346 0.10108944 0.1020179  0.08192872 0.08790331
- 0.10599291 0.10425983 0.09501596 0.09911878]
+score : 0.0
+[0.09753311 0.10002925 0.10379826 0.10192552 0.08558202 0.08127745
+ 0.11422036 0.11153243 0.10929769 0.09480391]
 
 30% 제거
-score : -0.012755094898871722
-[0.14898889 0.14885664 0.13235529 0.15608892 0.13421012 0.13919918
- 0.14030096]
+score : 0.02247191011235955
+[0.17569584 0.1307726  0.14014727 0.15188344 0.13347404 0.13590014
+ 0.1321266 ]
 
 # '''
