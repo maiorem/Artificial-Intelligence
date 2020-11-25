75b6fc27bac166cfe59ae76a5d958d84431aa081
Seyoung Hong <maiorem00@gmail.com>
2020-11-25 15:17:02 +0900

    Feature Importance

--- Study/ml/m23_FI2_diabetes.py
+++ Study/ml/m23_FI2_diabetes.py
@@ -5,7 +5,7 @@
 import numpy as np
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier, XGBRegressor
 from sklearn.metrics import r2_score, accuracy_score
 from sklearn.decomposition import PCA
 
@@ -13,28 +13,28 @@ dataset=load_diabetes()
 x=dataset.data
 y=dataset.target
 
-pca1=PCA(n_components=7)
-x=pca1.fit_transform(x)
+# pca1=PCA(n_components=7)
+# x=pca1.fit_transform(x)
 
 x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8)
 
-model=XGBClassifier(max_depth=4)
+model=XGBRegressor(max_depth=4)
 model.fit(x_train, y_train)
 
 y_predict=model.predict(x_test)
 
-print("score :", model.score(x_test, y_test))
+print("score :", r2_score(y_test, y_predict))
 print(model.feature_importances_)
 
 '''
 Default
-score : 0.0
-[0.09753311 0.10002925 0.10379826 0.10192552 0.08558202 0.08127745
- 0.11422036 0.11153243 0.10929769 0.09480391]
-
+score : 0.43378623375119496
+[0.02785363 0.08743825 0.15927461 0.07139384 0.0495851  0.05730768
+ 0.05833183 0.05959074 0.37548473 0.05373957]
+ 
 30% 제거
-score : 0.02247191011235955
-[0.17569584 0.1307726  0.14014727 0.15188344 0.13347404 0.13590014
- 0.1321266 ]
+score : 0.31112262069744623
+[0.28740177 0.09944782 0.13956098 0.23825209 0.06044487 0.0691275
+ 0.10576495]
 
 # '''
--- Study/ml/m23_FI3_boston.py
+++ Study/ml/m23_FI3_boston.py
@@ -5,7 +5,7 @@
 import numpy as np
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier, XGBRegressor
 from sklearn.metrics import r2_score
 from sklearn.decomposition import PCA
 
@@ -13,12 +13,12 @@ dataset=load_boston()
 x=dataset.data
 y=dataset.target
 
-pca1=PCA(n_components=12)
-x=pca1.fit_transform(x)
+# pca1=PCA(n_components=9)
+# x=pca1.fit_transform(x)
 
 x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8)
 
-model=XGBClassifier(max_depth=4)
+model=XGBRegressor(max_depth=4)
 model.fit(x_train, y_train)
 
 y_predict=model.predict(x_test)
@@ -28,18 +28,18 @@ print(model.feature_importances_)
 
 '''
 Default
-R2 : 0.6321515538653386
-[0.08557149 0.08373747 0.08950646 0.         0.09024695 0.08718417
- 0.08103242 0.08615365 0.07397702 0.08104043 0.07738417 0.06890872
- 0.09525705]
+R2 : 0.9090415131940273
+[0.02130319 0.00286287 0.01434059 0.01714036 0.0471935  0.15154535
+ 0.0130349  0.06681163 0.0114954  0.0272664  0.04258548 0.01972253
+ 0.5646978 ]
 
 0 제거
-R2 : 0.7853844867936511
-[0.09855166 0.08328348 0.07799172 0.07547305 0.08213176 0.0908768
- 0.08680062 0.08512171 0.07622263 0.07961532 0.08428118 0.07965012]
- 
+R2 : 0.8168341672424968
+[0.13871534 0.03327838 0.09786664 0.00845047 0.09369501 0.23535632
+ 0.02258671 0.07615449 0.08567709 0.03431726 0.14265576 0.03124638]
+
 30% 제거
-R2 : 0.606120909321445
-[0.12426724 0.10687196 0.11397473 0.10534319 0.1044957  0.11582578
- 0.11079461 0.11582579 0.10260104]
+R2 : 0.6682821217055774
+[0.15831138 0.02710617 0.15774179 0.02702889 0.13498697 0.35119942
+ 0.03275908 0.04039648 0.07046985]
 # '''
