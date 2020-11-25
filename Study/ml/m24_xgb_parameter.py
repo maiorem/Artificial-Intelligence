beb3fd5dd83932d4b36890dde6bf83c24c43ecf8
Seyoung Hong <maiorem00@gmail.com>
2020-11-25 15:53:09 +0900

    xgb parameter

--- Study/ml/m24_xgb_parameter.py
+++ Study/ml/m24_xgb_parameter.py
@@ -11,7 +11,32 @@ n_jobs : XGBoost를 실행하기 위한 병렬처리(쓰레드) 갯수
 colsample_bylevel : 트리 레벨별로 훈련데이터의 변수를 샘플링해주는 비율
 colsample_bytree : 각 트리마다의 feature 샘플링 비율
 '''
-
+from sklearn.datasets import load_boston
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier, XGBRegressor, plot_importance
 
-model=XGBRegressor()
+x, y=load_boston(return_X_y=True)
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.9)
+
+model=XGBRegressor(n_estimators=300, 
+                    learning_rate=1, 
+                    colsample_bytree=1, 
+                    colsample_bylevel=1,
+                    max_depth=5, 
+                    n_jobs=-1)
+
+model.fit(x_train, y_train)
+
+y_predict=model.predict(x_test)
+score=model.score(x_test, y_test)
+r2=r2_score(y_test, y_predict)
+print('score :', score)
+print('r2 :', r2)
+
+# XGBoost
+# r2 : 0.8916325276205544
+
+# Tensorflow
+# r2 :  0.8877071672091482
