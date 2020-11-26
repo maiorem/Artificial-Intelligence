9ce3fba3fb652dac3ca9b9879db6d983075e9685
Seyoung Hong <maiorem00@gmail.com>
2020-11-26 17:26:18 +0900

    Auto Keras

---
+++ Study/keras/keras65_autokeras.py
@@ -0,0 +1,32 @@
+# !pip install autokeras
+# !pip install git+https://github.com/keras-team/keras-tuner.git@1.0.2rc4
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.utils.data_utils import Sequence
+import autokeras as ak
+
+x=np.load('./data/keras64_x.npy')
+y=np.load('./data/keras64_y.npy')
+
+# print(x.shape) #(1736, 150, 150, 3)
+# print(y.shape) #(1736,)
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2)
+
+clf=ak.ImageClassifier(
+    overwrite=True,
+    max_trials=3
+)
+
+clf.fit(x_train, y_train, epochs=50)
+
+predicted_y=clf.predict(x_test)
+
+print('predicted_y :', predicted_y)
+print('evaluate() :', clf.evaluate(x_test, y_test))
+
+# max_trials=2 => evaluate() : [3.348659038543701, 0.5890804529190063]
+# max_trials=3 => 터짐
+
